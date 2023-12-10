49b8c185e2dcb25a8af573a15c3407ad75648187
Rong Huang <rongster3544@gmail.com>
2023-12-10 12:13:07 -0800

    2962. Count Subarrays Where Max Element Appears at Least K Times
    r

--- countSubarrays.js
+++ countSubarrays.js
@@ -1,3 +1,7 @@
+// Weekly Contest 375 q3
+// 2962. Count Subarrays Where Max Element Appears at Least K Times
+
+// sliding window
 var countSubarrays = function(nums, k) {
     let count = 0;
     let res = 0;
@@ -15,5 +19,67 @@ var countSubarrays = function(nums, k) {
             l++
         }
     }   
+    return res;
+};
+
+// hash table
+var countSubarrays2 = function(nums, k) {
+    let arr = [];
+    let map = {};
+    let max = Math.max(...nums);
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===max){
+            count++;
+        }
+        arr[i] = count;
+        if(map[count] === undefined){
+            map[count] = i;
+        }
+    }
+    let res = 0;
+    for(let i=0;i<nums.length;i++){
+        let target;
+        if(nums[i] === max){
+            target = arr[i]+k-1;
+        } else{
+            target = arr[i]+k;
+        }
+        if(map[target] !== undefined){
+            res+=nums.length-map[target];
+        }
+    }
+    return res;
+};
+
+// prefix sum + binary search
+let search = (l, r, arr, target)=>{
+    while(l<r){
+        let mid = Math.floor((l+r)/2);
+        if(arr[mid]<target){
+            l = mid+1;
+        } else {
+            r = mid;
+        }
+    }
+    return l;
+}
+var countSubarrays3 = function(nums, k) {
+    let max = Math.max(...nums);
+    let counts = [];
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===max){
+            count++;
+        }
+        counts[i]=count;
+    }
+    let res = 0;
+    for(let i=0;i<nums.length;i++){
+        if(counts[i] >= k){
+            let start = search(0, i, counts, counts[i]-k+1);
+            res+=start+1;
+        }
+    }
     return res;
 };
