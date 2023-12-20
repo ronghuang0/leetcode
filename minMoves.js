0094da80efc332c344503999e74972667ada11e5
Rong Huang <rongster3544@gmail.com>
2023-12-19 18:36:22 -0800

    462. Minimum Moves to Equal Array Elements II

---
+++ minMoves.js
@@ -0,0 +1,22 @@
+
+// find median
+var minMoves2 = function(nums) {
+    nums.sort((a,b)=>a-b);
+    let n = nums[Math.floor(nums.length/2)];
+    let res = 0;
+    for(let i=0;i<nums.length;i++){
+        res+= Math.abs(nums[i]-n);
+    }
+    return res;
+};
+
+// use difference between elements
+var minMoves2 = function(nums) {
+    nums.sort((a,b)=>a-b);
+    let n = nums.length/2;
+    let res = 0;
+    for(let i=0;i<n;i++){
+        res+= nums[nums.length-1-i] - nums[i];
+    }
+    return res;
+};
