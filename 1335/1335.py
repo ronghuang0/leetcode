e8b4e5e7db8b21f24ed028c7eb7d2864ac3c2b59
Rong Huang <rongster3544@gmail.com>
2024-01-08 21:58:44 -0800

    1335. Minimum Difficulty of a Job Schedule

--- 1335/1335.py
+++ 1335/1335.py
@@ -1,19 +1,43 @@
 # 1335. Minimum Difficulty of a Job Schedule
 
-from functools import cache
+# 3d dp
 class Solution:
     def minDifficulty(self, jobDifficulty, d):
         l = len(jobDifficulty)
-        @cache
-        def dfs(daysLeft, currMax, index):
+        if d > l:
+            return -1
+        cache = {}
+        def dfs(index, currMax, daysLeft):
             if index == l and daysLeft > 0:
                 return float('inf')
             if daysLeft==0:
                 return max([currMax] + jobDifficulty[index:])
-            sameDay = dfs(daysLeft, max(currMax, jobDifficulty[index]), index+1)
-            nextDay = currMax+dfs(daysLeft-1, jobDifficulty[index], index+1)
-            return min(sameDay, nextDay)
-        res = dfs(d-1, jobDifficulty[0], 1)
-        if res == float('inf'):
+            if (index, currMax, daysLeft) in cache:
+                return cache[(index, currMax, daysLeft)]
+            sameDay = dfs(index+1, max(currMax, jobDifficulty[index]), daysLeft)
+            nextDay = currMax+dfs(index+1, jobDifficulty[index], daysLeft-1)
+            cache[(index, currMax, daysLeft)] = min(sameDay, nextDay)
+            return cache[(index, currMax, daysLeft)]
+        return dfs(1, jobDifficulty[0], d-1)
+    
+# same logic, just with different base case
+class Solution:
+    def minDifficulty(self, jobDifficulty, d):
+        if d>len(jobDifficulty):
             return -1
-        return res
+        cache = {}
+        def dfs(index, currMax, daysLeft):
+            if index == len(jobDifficulty):
+                return 0 if daysLeft == 0 else float('inf')
+            if daysLeft == 0:
+                return float('inf')
+            if (index, currMax, daysLeft) in cache:
+                return cache[(index, currMax, daysLeft)]  
+            sameDay = dfs(index+1, max(currMax, jobDifficulty[index]), daysLeft)
+            nextDay = max(currMax, jobDifficulty[index])+dfs(index+1, -1, daysLeft-1)
+            cache[(index, currMax, daysLeft)] = min(sameDay, nextDay)
+            return cache[(index, currMax, daysLeft)] 
+        return dfs(0, -1, d)
+
+#2d dp
+    
