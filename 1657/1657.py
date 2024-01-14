c1b2aa75d2aae2ce16a1de4a77a4379c801d8d1c
Rong Huang <rongster3544@gmail.com>
2024-01-14 12:36:27 -0800

    # 1657. Determine if Two Strings Are Close

--- 1657/1657.py
+++ 1657/1657.py
@@ -1,4 +1,7 @@
 # 1657. Determine if Two Strings Are Close
+
+# hashmap 
+# time: O(n)  space: O(1) - length of 26
 from collections import Counter
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
@@ -11,3 +14,19 @@ class Solution:
         f1.sort()
         f2.sort()
         return f1 == f2
+    
+# arrays
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        freq1=[0]*26
+        freq2=[0]*26
+        for c in word1:
+            freq1[ord(c)-ord('a')]+=1
+        for c in word2:
+            freq2[ord(c)-ord('a')]+=1
+        for i in range(len(freq1)):
+            if (freq1[i] == 0 and freq2[i] > 0) or (freq2[i] == 0 and freq1[i] >0):
+                return False
+        freq1.sort()
+        freq2.sort()
+        return freq1==freq2
