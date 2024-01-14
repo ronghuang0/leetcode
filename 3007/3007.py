7e0c69b6c293ef1d6870c0ee42454d2db9888814
Rong Huang <rongster3544@gmail.com>
2024-01-14 13:03:39 -0800

    3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K

--- 3007/3007.py
+++ 3007/3007.py
@@ -1,6 +1,12 @@
 # Weekly Contest 380 q3
 # 3007. Maximum Number That Sum of the Prices Is Less Than or Equal to K
 
+# For a value num, for the range 1 to num, we can find the number of the 1s for each bit i
+# 0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
+# notice for bit 0 the length of a sequence is 2, 1->4, 2->8, 3->16 S=2^i
+# we can find number of complete sequences with (num+1)/S, multiplied by S/2 is 1s
+# we can find remainder with (num+1)%S. since each sequence starts with 0, we can do (num+1)%S - S/2
+import math
 class Solution:
     def findMaximumNumber(self, k: int, x: int) -> int:
         def findSum(num):
