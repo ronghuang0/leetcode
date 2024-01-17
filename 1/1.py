886762246bbce43037ffbc5858c1909d34b5ba67
Rong Huang <rongster3544@gmail.com>
2024-01-16 16:54:24 -0800

    1. Two Sum

---
+++ 1/1.py
@@ -0,0 +1,23 @@
+# 1. Two Sum
+
+# 2 pass
+class Solution:
+    def twoSum(self, nums, target):
+        map = {}
+        for i in range(len(nums)):
+            map[nums[i]]=i
+        for i in range(len(nums)):
+            t = target-nums[i]
+            if t in map and map[t] != i:
+                return [i, map[t]]
+        
+
+# 1 pass
+class Solution:
+    def twoSum(self, nums, target):
+        map = {}
+        for i in range(len(nums)):
+            t = target-nums[i]
+            if t in map:
+                return [i, map[t]]
+            map[nums[i]]=i
