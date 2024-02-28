f1d10e93b9cee14d418589402fa346d5f7706282
Rong Huang <rongster3544@gmail.com>
2024-02-27 23:28:18 -0800

    1245. Tree Diameter

--- 1245/1245.py
+++ 1245/1245.py
@@ -1,5 +1,8 @@
 #1245. Tree Diameter
 
+from collections import defaultdict
+
+# two dfs calls
 class Solution:
     def treeDiameter(self, edges):
         adj = defaultdict(list)
@@ -22,3 +25,29 @@ class Solution:
         length, node = dfs(0, set({0}))
         resLength, resNode = dfs(node, set({node}))
         return resLength
+
+# dfs with heights
+class Solution:
+    def treeDiameter(self, edges) -> int:
+        adj = defaultdict(list)
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        res = 0
+        def dfs(node, parent):
+            longest = -1
+            secondLongest = -1
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                length = dfs(nei, node)
+                if length > longest:
+                    secondLongest = longest
+                    longest = length
+                elif length > secondLongest:
+                    secondLongest = length
+            nonlocal res
+            res = max(res, longest+secondLongest+2)
+            return longest+1
+        dfs(0, -1)
+        return res  
