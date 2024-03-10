d101bf63f151576030a0619cfa3f1944d43bcd5a
Rong Huang <rongster3544@gmail.com>
2024-03-09 19:43:16 -0800

    787. Cheapest Flights within K Stops

--- 787/787.py
+++ 787/787.py
@@ -19,3 +19,29 @@ class Solution:
             for nei, p in adj[node]:
                 heapq.heappush(h, (price+p, nei, s+1))
         return -1
+    
+# bfs
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        dist = [float('inf')]*n
+        dist[src]=0
+        adj = defaultdict(list)
+        for u,v,p in flights:
+            adj[u].append((v,p))
+        level = [src]
+        length = 0
+        while length < k+1:
+            next = []
+            tempDist = dist.copy()
+            for u in level:
+                for v,p in adj[u]:
+                    if dist[u]+p < tempDist[v]:
+                        tempDist[v] = dist[u]+p
+                        next.append(v)
+            length+=1
+            level = next
+            dist=tempDist
+            
+        if dist[dst] == float('inf'):
+            return -1
+        return dist[dst]
