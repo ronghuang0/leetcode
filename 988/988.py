3e7c4efe869b916e1648eb58281f8ee0be10bcdc
Rong Huang <rongster3544@gmail.com>
2024-04-17 13:16:31 -0700

    988. Smallest String Starting From Leaf

--- 988/988.py
+++ 988/988.py
@@ -4,14 +4,11 @@ class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
         res=None
         def dfs(curr, node):
-            char = chr(ord('a')+int(node.val))
-            curr+=char
+            curr=chr(ord('a')+node.val)+curr
             if not node.left and not node.right:
                 nonlocal res
-                if not res:
-                    res=curr[::-1]
-                else:
-                    res=min(res, curr[::-1])
+                if not res or curr<res:
+                    res=curr
             if node.left:
                 dfs(curr, node.left)
             if node.right:
