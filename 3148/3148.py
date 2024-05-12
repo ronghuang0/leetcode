7f9eccc2516c0d74b8dc15265d0af2279664d494
Rong Huang <rongster3544@gmail.com>
2024-05-12 16:12:56 -0700

    3148. Maximum Difference Score in a Grid

--- 3148/3148.py
+++ 3148/3148.py
@@ -4,17 +4,14 @@ class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        biggest=[[0]*n for _ in range(m)]
-        for i in range(m-1, -1,-1):
-            for j in range(n-1, -1,-1):
-                if i+1<=m-1:
-                    biggest[i][j] = max(grid[i+1][j], biggest[i+1][j])
-                if j+1<=n-1:
-                    biggest[i][j]=max(biggest[i][j],grid[i][j+1],biggest[i][j+1])
         res=float('-inf')
         for i in range(m-1, -1, -1):
-            for j in range(n-1, -1,-1):
-                if i==m-1 and j==n-1:
-                    continue
-                res=max(res, biggest[i][j]-grid[i][j])
+            for j in range(n-1, -1, -1):
+                greatest=float('-inf')
+                if i<m-1:
+                    greatest=max(greatest, grid[i+1][j])
+                if j<n-1:
+                    greatest=max(greatest, grid[i][j+1])
+                res=max(res, greatest-grid[i][j])
+                grid[i][j]=max(greatest, grid[i][j])
         return res
