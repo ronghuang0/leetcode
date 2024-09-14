2916a0a3499bd8e218748acb0b205b2b3a404854
Rong Huang <rongster3544@gmail.com>
2024-09-14 05:57:51 -0700

    2419. Longest Subarray With Maximum Bitwise AND

---
+++ 2419/2419.py
@@ -0,0 +1,17 @@
+# 2419. Longest Subarray With Maximum Bitwise AND
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_val = ans = current_streak = 0
+        for num in nums:
+            if max_val < num:
+                max_val = num
+                ans = current_streak = 0
+
+            if max_val == num:
+                current_streak += 1
+            else:
+                current_streak = 0
+
+            ans = max(ans, current_streak)
+        return ans
