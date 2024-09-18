cd1bf0c8f5a98820368f743bd107bcb75f1441db
Rong Huang <rongster3544@gmail.com>
2024-09-18 11:36:26 -0700

    179. Largest Number

--- 179/179.py
+++ 179/179.py
@@ -1,2 +1,18 @@
 # 179. Largest Number
 
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def comp(a,b):
+            a=str(a)
+            b=str(b)
+            if (a+b) > (b+a):
+                return -1
+            return 1
+        
+        nums.sort(key=cmp_to_key(comp))
+        res=''
+        for num in nums:
+            res+=str(num)
+        if res[0]=='0':
+            return '0'
+        return res
